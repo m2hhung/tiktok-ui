9053f4924589fac39c9d0633ba769989f72120da
m2hhu <m2h.hung@gmail.com>
2022-06-15 16:57:30 +0700

    #61

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Upload from '@/pages/Upload';
+import { HeaderOnly } from '@/components/Layout';
+
+export const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+export const privateRoutes = [];
